2f00d64c1fb2850c4b0fcbca2e3698fada09bfcc
Chamele-on-off <mot124@yandex.ru>
2025-07-05 08:48:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ class DB:
     @staticmethod
     def get_user(username):
         users = DB.get_users()
-        return next((u for u in users if u['username'] == username), None
+        return next((u for u in users if u['username'] == username), None)
 
     @staticmethod
     def save_user(username, email, password, role='student', is_active=True):
@@ -115,7 +115,7 @@ class DB:
     @staticmethod
     def get_lesson(lesson_id):
         lessons = DB.get_lessons()
-        return next((l for l in lessons if l['id'] == lesson_id), None
+        return next((l for l in lessons if l['id'] == lesson_id), None)
 
     @staticmethod
     def save_lesson(title, description, teacher, schedule, duration=60, program_type='languages', students=None, recurrence=None):
